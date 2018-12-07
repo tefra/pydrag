b7ce4295a667eaf1417ec3326416e1b28a887d28
Chris <chris@komposta.net>
2018-12-07 21:22:34 +0200

    Flatten project structure

--- pydrag/__init__.py
+++ pydrag/__init__.py
@@ -1,3 +1,8 @@
-from pydrag import lastfm
+from pydrag.lastfm.models.config import config, configure
+from pydrag.lastfm.models.album import Album
+from pydrag.lastfm.models.artist import Artist
+from pydrag.lastfm.models.track import Tag
+from pydrag.lastfm.models.track import Track
+from pydrag.lastfm.models.user import User
 
-__all__ = ["lastfm"]
+__all__ = ["User", "Track", "Album", "Artist", "Tag", "config", "configure"]
--- pydrag/lastfm/__init__.py
+++
@@ -1,8 +0,0 @@
-from pydrag.lastfm.models.config import config, configure
-from pydrag.lastfm.models.album import Album
-from pydrag.lastfm.models.artist import Artist
-from pydrag.lastfm.models.track import Tag
-from pydrag.lastfm.models.track import Track
-from pydrag.lastfm.models.user import User
-
-__all__ = ["User", "Track", "Album", "Artist", "Tag", "config", "configure"]
