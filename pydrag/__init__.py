f108face4ce3e5f5bdbc371c44739f0049869fc9
Chris <chris@komposta.net>
2018-12-10 22:52:24 +0200

    Be a little less opinionated
    I still use Pipenv for my env :)

--- pydrag/__init__.py
+++ pydrag/__init__.py
@@ -6,6 +6,15 @@ from pydrag.models.track import Track
 from pydrag.models.user import User
 from pydrag.models.auth import AuthToken, AuthSession
 
+
+try:
+    import dotenv
+except ImportError:
+    # tox doesn't load python-dotenv
+    pass
+else:
+    dotenv.load_dotenv()
+
 configure = Config.instance
 
 __all__ = [
--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import setup
 
-install_requires = ["attrs == 18.2.0", "requests == 2.20.1"]
-
 if __name__ == "__main__":
     setup(
         packages=["pydrag"],
         package_dir={"pydrag": "pydrag"},
-        install_requires=install_requires,
+        install_requires=[
+            "attrs == 18.2.0",
+            "requests == 2.21.0",
+            "python-dotenv == 0.10.0",
+        ],
+        extras_require={
+            "dev": [
+                "pre-commit",
+                "pytest",
+                "pytest-cov",
+                "codecov",
+                "vcrpy",
+                "tox",
+                "dot",
+            ],
+            "docs": ["sphinx", "sphinx-rtd-theme", "sphinx-autodoc-typehints"],
+        },
     )
